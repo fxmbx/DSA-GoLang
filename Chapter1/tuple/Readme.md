8c5b3228ab6bc49f4089ba0b61ba9cb03023d2c2
fxmbx <funbiolaore@gmail.com>
2022-09-20 05:46:08 +0100

    tuples

--- Chapter1/tuple/Readme.md
+++ Chapter1/tuple/Readme.md
@@ -1,1 +1,1 @@
-a tuple is a finite sorted list of elements. It is a data structure that groups data
+a tuple is a finite sorted list of elements. It is a data structure that groups data (pair or an ordered set of values)
--- Chapter1/tuple/tuple.go
+++ Chapter1/tuple/tuple.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import "fmt"
 
+type StudentTuple struct {
+	regNo, name any
+}
+
+func main() {
+	var square int
+	var cube int
+	square, cube = powerSeries(3)
+	fmt.Println("Square: ", square, "cube: ", cube)
+
+	student1 := &StudentTuple{171766, "Olaore oluwafunmibi olumuyiwa"}
+	fmt.Printf("stuent name: %s, student reg number: %d", student1.name, student1.regNo)
+}
+
+func powerSeries(a int) (int, int) {
+	return a * a, a * a * a
 }
